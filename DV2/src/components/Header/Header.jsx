133f820dad7412c70a8c62cbdbac6f46d444915a
LooiJingHao <jinghao101913@gmail.com>
2026-05-12 12:13:08 +0800

    added introduction paragraph

--- DV2/src/components/Header/Header.jsx
+++ DV2/src/components/Header/Header.jsx
@@ -6,6 +6,10 @@ export default function Header(){
                 <h1>The King of Fruits: A Comprehensive <br />Analysis of Malaysia's Durian Industry</h1>
                 <p>Exploring durian production, geography, exports, prices, and Malaysia's role across <br />the world durain market</p>
             </div>
+            <div className="intro-parag">
+                <h2>Introduction</h2>
+                <p>Malaysia's durian industry has rapidly evolved from a local delicacy into a globally recognised agricultural export. Driven by rising international demand <strong>particularly from China</strong> premium durian varieties now contribute significantly to Malaysia's economy through <strong>production</strong>, <strong>exports</strong>, and tourism. However, rapid industry growth also raises concerns surrounding sustainability and long-term agricultural resilience.</p>
+            </div>
             <div className="KPI-Section">
                 <div className="box-container">
                     
