54ac88d8bf9a38b79a30a333495a0a642b804f3b
LooiJingHao <jinghao101913@gmail.com>
2026-05-21 16:47:43 +0800

    .

--- DV2/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/FIT2179-DV2/',
-  plugins: [react()],
-})
